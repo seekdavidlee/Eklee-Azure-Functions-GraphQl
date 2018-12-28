de4cc549ccd04fcfd752b89de9fde7c183cb5fe1
David Lee <seekdavidlee@gmail.com>
2018-12-28 15:17:13 -0600

    Minor update to upgrade all nuget packages to the latest stable versions. There was also an update to fix an issue with having more than one Module defined.

---
+++ Examples/Eklee.Azure.Functions.GraphQl.Example/MyModule2.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Eklee.Azure.Functions.GraphQl.Example.HttpMocks;
+
+namespace Eklee.Azure.Functions.GraphQl.Example
+{
+	public class MyModule2 : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<PublisherMockRepository>().As<IHttpMockRepository<Publisher>>().SingleInstance();
+		}
+	}
+}

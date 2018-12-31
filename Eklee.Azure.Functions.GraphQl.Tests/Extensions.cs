e16719cf2809bca897eaec845e94f7ccebda09bc
David Lee <seekdavidlee@gmail.com>
2018-12-30 21:25:34 -0600

    Fix query issue due to comparison which requires enabling scan in header.

---
+++ Eklee.Azure.Functions.GraphQl.Tests/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eklee.Azure.Functions.GraphQl.Tests
+{
+	public static class Extensions
+	{
+		public static DateTime ToUtc(this DateTime date)
+		{
+			return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, 0, DateTimeKind.Utc);
+		}
+	}
+}
--- Eklee.Azure.Functions.GraphQl.Tests/Repository/DocumentDbRepositoryQueryTests.cs
+++ Eklee.Azure.Functions.GraphQl.Tests/Repository/DocumentDbRepositoryQueryTests.cs
@@ -37,7 +37,7 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 					Category = "cat 1",
 					Description = "Ha ha ha",
 					Level = 4,
-					Effective = new DateTime(2017, 1, 1),
+					Effective = new DateTime(2017, 1, 1).ToUtc(),
 					TypeId = Guid.Parse("AF359AD6-9B8E-43F7-B898-35CEB400051A")
 				},
 				new DocumentDbFoo3
@@ -47,7 +47,7 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 					Category = "cat 1",
 					Description = "Ra ha ha",
 					Level = 2,
-					Effective = new DateTime(2018, 1, 1),
+					Effective = new DateTime(2018, 1, 1).ToUtc(),
 					TypeId = Guid.Parse("9B522321-B689-43EC-A3DC-DC17EE2A42DD")
 				},
 				new DocumentDbFoo3
@@ -57,8 +57,8 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 					Category = "cat 2",
 					Description = "Na ha ha",
 					Level = 3,
-					Effective = new DateTime(2016, 1, 1),
-					Expires = new DateTime(2018, 5, 1),
+					Effective = new DateTime(2016, 1, 1).ToUtc(),
+					Expires = new DateTime(2018, 5, 1).ToUtc(),
 					TypeId = Guid.Parse("CC562BC9-C2E5-46D8-B701-E5D57F5800B8")
 				},
 				new DocumentDbFoo3
@@ -68,7 +68,7 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 					Category = "cat 1",
 					Description = "Ba ha Ba",
 					Level = 2,
-					Effective = new DateTime(2014, 12, 1),
+					Effective = new DateTime(2014, 12, 1).ToUtc(),
 					TypeId = Guid.Parse("4AB97B79-37DB-4AB7-BC32-D3EB1780C8AF")
 				},
 				new DocumentDbFoo3
@@ -78,8 +78,8 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 					Category = "cat 5",
 					Description = "Sa Sa Ba",
 					Level = 6,
-					Effective = new DateTime(2014, 1, 1),
-					Expires = new DateTime(2015, 12, 31),
+					Effective = new DateTime(2014, 1, 1).ToUtc(),
+					Expires = new DateTime(2015, 12, 31).ToUtc(),
 					TypeId = Guid.Parse("9B522321-B689-43EC-A3DC-DC17EE2A42DD")
 				}
 			};
@@ -91,7 +91,7 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 		}
 
 		[Fact]
-		public async Task CanQueryWith_StartsWith()
+		public async Task CanQueryWithStartsWith()
 		{
 			await Seed();
 
@@ -115,7 +115,7 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 		}
 
 		[Fact]
-		public async Task CanQueryWith_EndsWith()
+		public async Task CanQueryWithEndsWith()
 		{
 			await Seed();
 
@@ -138,7 +138,7 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 		}
 
 		[Fact]
-		public async Task CanQueryWith_Contains()
+		public async Task CanQueryWithContains()
 		{
 			await Seed();
 
@@ -162,7 +162,7 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 		}
 
 		[Fact]
-		public async Task CanQueryWith_TwoArgsOfTypesStringAndInt()
+		public async Task CanQueryWithTwoArgsOfTypesStringAndInt()
 		{
 			await Seed();
 
@@ -227,6 +227,295 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Repository
 			var item2 = results.Single(x => x.Id == "1");
 			item2.Category.ShouldBe("cat 1");
 			item2.TypeId.ShouldBe(typeId);
+		}
+
+		[Fact]
+		public async Task CanQueryByDateTimeWithEqual()
+		{
+			await Seed();
+
+			var date = DateTime.Parse("2018-01-01");
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = date,
+						Comparison = Comparisons.Equal
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Effective"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(1);
+
+			var item2 = results.Single(x => x.Id == "2");
+			item2.Category.ShouldBe("cat 1");
+			item2.Effective.ShouldBe(date);
+		}
+
+		[Fact]
+		public async Task CanQueryByDateTimeWithGreaterEqualThan()
+		{
+			await Seed();
+
+			var date = DateTime.Parse("2017-01-01");
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = date,
+						Comparison = Comparisons.GreaterEqualThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Effective"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(2);
+
+			var item1 = results.Single(x => x.Id == "1");
+			item1.Category.ShouldBe("cat 1");
+
+			var item2 = results.Single(x => x.Id == "2");
+			item2.Category.ShouldBe("cat 1");
+		}
+
+		[Fact]
+		public async Task CanQueryByDateTimeWithGreaterThan()
+		{
+			await Seed();
+
+			var date = DateTime.Parse("2017-01-01");
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = date,
+						Comparison = Comparisons.GreaterThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Effective"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(1);
+
+			var item2 = results.Single(x => x.Id == "2");
+			item2.Category.ShouldBe("cat 1");
+		}
+
+		[Fact]
+		public async Task CanQueryByDateTimeWithLessThan()
+		{
+			await Seed();
+
+			var date = DateTime.Parse("2014-01-02");
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = date,
+						Comparison = Comparisons.LessThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Effective"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(1);
+
+			var item2 = results.Single(x => x.Id == "5");
+			item2.Name.ShouldBe("Bar 5");
+		}
+
+		[Fact]
+		public async Task CanQueryByDateTimeWithLessEqualThan()
+		{
+			await Seed();
+
+			var date = DateTime.Parse("2014-12-01");
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = date,
+						Comparison = Comparisons.LessEqualThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Effective"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(2);
+
+			var item1 = results.Single(x => x.Id == "4");
+			item1.Name.ShouldBe("Foo 4");
+
+			var item2 = results.Single(x => x.Id == "5");
+			item2.Name.ShouldBe("Bar 5");
+		}
+
+		[Fact]
+		public async Task CanQueryByIntWithEqual()
+		{
+			await Seed();
+
+			var level = 6;
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = level,
+						Comparison = Comparisons.Equal
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Level"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(1);
+
+			var item2 = results.Single(x => x.Id == "5");
+			item2.Level.ShouldBe(level);
+		}
+
+		[Fact]
+		public async Task CanQueryByIntWithGreaterThan()
+		{
+			await Seed();
+
+			var level = 4;
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = level,
+						Comparison = Comparisons.GreaterThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Level"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(1);
+
+			var item2 = results.Single(x => x.Id == "5");
+			item2.Level.ShouldBe(6);
+		}
+
+		[Fact]
+		public async Task CanQueryByIntWithGreaterEqualThan()
+		{
+			await Seed();
+
+			var level = 4;
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = level,
+						Comparison = Comparisons.GreaterEqualThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Level"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(2);
+
+			var item1 = results.Single(x => x.Id == "1");
+			item1.Level.ShouldBe(4);
+
+			var item2 = results.Single(x => x.Id == "5");
+			item2.Level.ShouldBe(6);
+		}
+
+		[Fact]
+		public async Task CanQueryByIntWithLessThan()
+		{
+			await Seed();
+
+			var level = 3;
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = level,
+						Comparison = Comparisons.LessThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Level"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(2);
+
+			var item1 = results.Single(x => x.Id == "2");
+			item1.Level.ShouldBe(2);
+
+			var item2 = results.Single(x => x.Id == "4");
+			item2.Level.ShouldBe(2);
+		}
+
+		[Fact]
+		public async Task CanQueryByIntWithLessEqualThan()
+		{
+			await Seed();
+
+			var level = 3;
+			QueryParameter[] args = {
+				new QueryParameter
+				{
+					ContextValue = new ContextValue
+					{
+						Value = level,
+						Comparison = Comparisons.LessEqualThan
+					},
+					MemberModel = new ModelMember(_type, _accessor,
+						_members.Single(x=> x.Name == "Level"), false)
+				}
+			};
+
+			var results = (await DocumentDbRepository.QueryAsync<DocumentDbFoo3>("test", args)).ToList();
+
+			results.Count.ShouldBe(3);
+
+			var item1 = results.Single(x => x.Id == "2");
+			item1.Level.ShouldBe(2);
+
+			var item2 = results.Single(x => x.Id == "4");
+			item2.Level.ShouldBe(2);
+
+			var item3 = results.Single(x => x.Id == "3");
+			item3.Level.ShouldBe(3);
 		}
 
 		public void Dispose()

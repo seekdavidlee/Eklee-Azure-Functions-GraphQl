6abf14860291328dd05eb0537c60f9dca9916ab5
David Lee <seekdavidlee@gmail.com>
2019-04-14 06:21:39 -0500

    More unit tests for model connections

--- Eklee.Azure.Functions.GraphQl.Tests/Connections/ConnectionFieldResolverTests.cs
+++ Eklee.Azure.Functions.GraphQl.Tests/Connections/ConnectionFieldResolverTests.cs
@@ -138,17 +138,28 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Connections
 			m2_1.ShouldNotBeNull();
 			m3_1.ShouldNotBeNull();
 
-			edges[0].SourceId.ShouldBe(srcId);
-			edges[0].DestinationId.ShouldBe(destId);
+			AssertEdge(edges[0], srcId, destId);
+		}
 
-			edges[0].MetaFieldName.ShouldNotBeNull();
-			edges[0].MetaFieldName.ShouldNotBeEmpty();
+		private void AssertEdge(ConnectionEdge edge, string srcId, string destId)
+		{
+			edge.Id.ShouldNotBeNull();
+			edge.Id.ShouldNotBeEmpty();
 
-			edges[0].MetaType.ShouldNotBeNull();
-			edges[0].MetaType.ShouldNotBeEmpty();
+			edge.FieldName.ShouldNotBeNull();
+			edge.FieldName.ShouldNotBeEmpty();
 
-			edges[0].MetaValue.ShouldNotBeNull();
-			edges[0].MetaValue.ShouldNotBeEmpty();
+			edge.SourceId.ShouldBe(srcId);
+			edge.DestinationId.ShouldBe(destId);
+
+			edge.MetaFieldName.ShouldNotBeNull();
+			edge.MetaFieldName.ShouldNotBeEmpty();
+
+			edge.MetaType.ShouldNotBeNull();
+			edge.MetaType.ShouldNotBeEmpty();
+
+			edge.MetaValue.ShouldNotBeNull();
+			edge.MetaValue.ShouldNotBeEmpty();
 		}
 
 		[Fact]
@@ -211,6 +222,181 @@ namespace Eklee.Azure.Functions.GraphQl.Tests.Connections
 
 			edges[0].MetaValue.ShouldNotBeNull();
 			edges[0].MetaValue.ShouldNotBeEmpty();
+		}
+
+
+		[Fact]
+		public void CanResolve3EdgeConnection()
+		{
+			const string srcId = "conn_1";
+			const string destId1 = "conn_2_1";
+			const string destId2 = "conn_2_2";
+			const string destId3 = "conn_2_3";
+			int counter = 0;
+
+			var model = new ModelWith3Connections
+			{
+				Id = srcId,
+				Field1 = "fas",
+				Edge1 = new ModelWith3ConnectionsEdge
+				{
+					Id = destId1,
+					Field1 = "das one",
+					Other = new ModelWith3ConnectionsOther { Id = destId1 }
+				},
+				Edge2 = new ModelWith3ConnectionsEdge
+				{
+					Id = destId2,
+					Field1 = "das two",
+					Other = new ModelWith3ConnectionsOther { Id = destId2 }
+				},
+				Edge3 = new ModelWith3ConnectionsEdge
+				{
+					Id = destId3,
+					Field1 = "das three",
+					Other = new ModelWith3ConnectionsOther { Id = destId3 }
+				}
+			};
+
+			ModelWith3Connections m1 = null;
+			ModelWith3ConnectionsOther m2_1 = null;
+			ModelWith3ConnectionsOther m2_2 = null;
+			ModelWith3ConnectionsOther m2_3 = null;
+
+			var edges = _connectionFieldResolver.HandleConnectionEdges(model, (entity) =>
+			{
+				if (entity is ModelWith3Connections em1)
+				{
+					if (em1.Id == srcId) m1 = em1;
+				}
+
+				if (entity is ModelWith3ConnectionsOther em2)
+				{
+					if (em2.Id == destId1) m2_1 = em2;
+					if (em2.Id == destId2) m2_2 = em2;
+					if (em2.Id == destId3) m2_3 = em2;
+				}
+
+				counter++;
+			});
+
+			counter.ShouldBe(4);
+
+			edges.ShouldNotBeNull();
+			edges.Count.ShouldBe(3);
+
+			m1.ShouldNotBeNull();
+			m2_1.ShouldNotBeNull();
+			m2_2.ShouldNotBeNull();
+			m2_3.ShouldNotBeNull();
+
+			int errorCount = 0;
+
+			edges.ForEach(edge =>
+			{
+				if (edge.DestinationId == destId1)
+				{
+					AssertEdge(edge, srcId, destId1);
+					return;
+				}
+
+				if (edge.DestinationId == destId2)
+				{
+					AssertEdge(edge, srcId, destId2);
+					return;
+				}
+
+				if (edge.DestinationId == destId3)
+				{
+					AssertEdge(edge, srcId, destId3);
+					return;
+				}
+
+				errorCount++;
+
+			});
+
+			errorCount.ShouldBe(0);
+		}
+
+		[Fact]
+		public void CanResolveWithAtLeastOneNullEdgeConnection()
+		{
+			const string srcId = "conn_1";
+			const string destId1 = "conn_2_1";
+			const string destId2 = "conn_2_2";
+
+			int counter = 0;
+
+			var model = new ModelWith3Connections
+			{
+				Id = srcId,
+				Field1 = "fas",
+				Edge1 = new ModelWith3ConnectionsEdge
+				{
+					Id = destId1,
+					Field1 = "das one",
+					Other = new ModelWith3ConnectionsOther { Id = destId1 }
+				},
+				Edge2 = new ModelWith3ConnectionsEdge
+				{
+					Id = destId2,
+					Field1 = "das two",
+					Other = new ModelWith3ConnectionsOther { Id = destId2 }
+				},
+				Edge3 = null
+			};
+
+			ModelWith3Connections m1 = null;
+			ModelWith3ConnectionsOther m2_1 = null;
+			ModelWith3ConnectionsOther m2_2 = null;
+
+			var edges = _connectionFieldResolver.HandleConnectionEdges(model, (entity) =>
+			{
+				if (entity is ModelWith3Connections em1)
+				{
+					if (em1.Id == srcId) m1 = em1;
+				}
+
+				if (entity is ModelWith3ConnectionsOther em2)
+				{
+					if (em2.Id == destId1) m2_1 = em2;
+					if (em2.Id == destId2) m2_2 = em2;
+				}
+
+				counter++;
+			});
+
+			counter.ShouldBe(3);
+
+			edges.ShouldNotBeNull();
+			edges.Count.ShouldBe(2);
+
+			m1.ShouldNotBeNull();
+			m2_1.ShouldNotBeNull();
+			m2_2.ShouldNotBeNull();
+
+			int errorCount = 0;
+
+			edges.ForEach(edge =>
+			{
+				if (edge.DestinationId == destId1)
+				{
+					AssertEdge(edge, srcId, destId1);
+					return;
+				}
+
+				if (edge.DestinationId == destId2)
+				{
+					AssertEdge(edge, srcId, destId2);
+					return;
+				}
+
+				errorCount++;
+
+			});
+
+			errorCount.ShouldBe(0);
 		}
 	}
 }
---
+++ Eklee.Azure.Functions.GraphQl.Tests/Models/ModelWith3ConnectionsEdge.cs
@@ -0,0 +1,15 @@
+﻿using Eklee.Azure.Functions.GraphQl.Connections;
+
+namespace Eklee.Azure.Functions.GraphQl.Tests.Models
+{
+	public class ModelWith3ConnectionsEdge
+	{
+		[ConnectionEdgeDestinationKey]
+		public string Id { get; set; }
+
+		public string Field1 { get; set; }
+
+		[ConnectionEdgeDestination]
+		public ModelWith3ConnectionsOther Other { get; set; }
+	}
+}

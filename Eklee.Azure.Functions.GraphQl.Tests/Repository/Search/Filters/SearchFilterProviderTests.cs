975d80b5235cfa71af5ade53c39a5dbc919e0178
David Lee <seekdavidlee@gmail.com>
2019-08-11 20:39:20 -0500

    Add unit tests for SearchFilterProviderTests.

---
+++ Eklee.Azure.Functions.GraphQl.Tests/Repository/Search/Filters/SearchFilterProviderTests.cs
@@ -0,0 +1,108 @@
+﻿using Eklee.Azure.Functions.GraphQl.Repository.Search;
+using Eklee.Azure.Functions.GraphQl.Repository.Search.Filters;
+using FastMember;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Eklee.Azure.Functions.GraphQl.Tests.Repository.Search.Filters
+{
+	public class MockItem
+	{
+		public int IntValue { get; set; }
+		public string StringValue { get; set; }
+	}
+
+	[Trait(Constants.Category, Constants.UnitTests)]
+	public class SearchFilterProviderTests
+	{
+		private readonly TypeAccessor _searchModeltypeAccessor;
+		private readonly Type _searchModelType;
+		private readonly List<QueryParameter> _queryParams = new List<QueryParameter>();
+		private readonly MemberSet _members;
+		public SearchFilterProviderTests()
+		{
+			var mockItemType = typeof(MockItem);
+			var mockItemTypeAccessor = TypeAccessor.Create(mockItemType);
+
+			_members = mockItemTypeAccessor.GetMembers();
+			_searchModelType = typeof(SearchModel);
+			_searchModeltypeAccessor = TypeAccessor.Create(_searchModelType);
+		}
+
+		[Fact]
+		public void NoMatchShouldReturnNull()
+		{
+			var searchFilterProvider = new SearchFilterProvider(null);
+			searchFilterProvider.GenerateStringFilter(_queryParams, _members).ShouldBeNull();
+		}
+
+		[Fact]
+		public void ThrowArgumentExceptionIfInputNotValid()
+		{
+			AddQueryParameterWithValidModelMember(new ContextValue
+			{
+				Values = new List<object>
+				{
+					new SearchFilterModel
+					{
+						FieldName = "Foo"
+					}
+				}
+			});
+
+			var searchFilterProvider = new SearchFilterProvider(null);
+			Should.Throw<ArgumentException>(() => searchFilterProvider.GenerateStringFilter(_queryParams, _members));
+		}
+
+		private void AddQueryParameterWithValidModelMember(ContextValue contextValue)
+		{
+			var member = _searchModeltypeAccessor.GetMembers().Single(m => m.Name == "Filters");
+			var queryParameter = new QueryParameter
+			{
+				MemberModel = new ModelMember(_searchModelType, _searchModeltypeAccessor, member, false),
+				ContextValue = contextValue
+			};
+
+			_queryParams.Add(queryParameter);
+		}
+
+		[Fact]
+		public void ShouldReturnNullIfNoMatchingFiltersFound()
+		{
+			var searchFilterProvider = new SearchFilterProvider(new List<ISearchFilter>());
+
+			searchFilterProvider.GenerateStringFilter(_queryParams, _members).ShouldBeNull();
+		}
+
+		private SearchFilterProvider GetSearchFilterProviderWithFilters()
+		{
+			return new SearchFilterProvider(new List<ISearchFilter>
+			{
+				new StringSearchFilter()
+			});
+		}
+
+		[Fact]
+		public void ShouldReturnIfStringFilterMatched()
+		{
+			AddQueryParameterWithValidModelMember(new ContextValue
+			{
+				Values = new List<object>
+				{
+					new SearchFilterModel
+					{
+						FieldName = "StringValue",
+						Comprison = Comparisons.Equal,
+						Value = "One"
+					}
+				}
+			});
+
+			var sp = GetSearchFilterProviderWithFilters();
+			sp.GenerateStringFilter(_queryParams, _members).ShouldNotBeNullOrEmpty();
+		}
+	}
+}

6c14bc542e7b09ef66f21a3ce5cdb5d0a6d98681
David Lee <seekdavidlee@gmail.com>
2020-10-03 11:44:09 -0500

    move function

--- Reset.ps1
+++ Reset.ps1
@@ -43,37 +43,37 @@ function ResetSearch {
 	}
 }
 
-function GenerateMasterKeyAuthorizationSignature {
-	[CmdletBinding()]
-	Param
-	(
-		[Parameter(Mandatory = $true)][String]$verb,
-		[Parameter(Mandatory = $true)][String]$resourceLink,
-		[Parameter(Mandatory = $true)][String]$resourceType,
-		[Parameter(Mandatory = $true)][String]$dateTime,
-		[Parameter(Mandatory = $true)][String]$key,
-		[Parameter(Mandatory = $true)][String]$keyType,
-		[Parameter(Mandatory = $true)][String]$tokenVersion
-	)
-
-	If ($resourceLink -eq $resourceType) { 
-		$resourceLink = "" 
-	}
-
-	$hmacSha256 = New-Object System.Security.Cryptography.HMACSHA256
-	$hmacSha256.Key = [System.Convert]::FromBase64String($key)
- 
-	$payLoad = "$($verb.ToLowerInvariant())`n$($resourceType.ToLowerInvariant())`n$resourceLink`n$($dateTime.ToLowerInvariant())`n`n"
-	$hashPayLoad = $hmacSha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($payLoad))
-	$signature = [System.Convert]::ToBase64String($hashPayLoad);
- 
-	[System.Web.HttpUtility]::UrlEncode("type=$keyType&ver=$tokenVersion&sig=$signature")
-}
-
 function ResetDocumentDb {
 	param(
 		[Parameter(Mandatory = $True)][string]$ResourceGroupName, 
 		[Parameter(Mandatory = $True)][string]$AccountName)
+
+	function GenerateMasterKeyAuthorizationSignature {
+		[CmdletBinding()]
+		Param
+		(
+			[Parameter(Mandatory = $true)][String]$verb,
+			[Parameter(Mandatory = $true)][String]$resourceLink,
+			[Parameter(Mandatory = $true)][String]$resourceType,
+			[Parameter(Mandatory = $true)][String]$dateTime,
+			[Parameter(Mandatory = $true)][String]$key,
+			[Parameter(Mandatory = $true)][String]$keyType,
+			[Parameter(Mandatory = $true)][String]$tokenVersion
+		)
+
+		If ($resourceLink -eq $resourceType) { 
+			$resourceLink = "" 
+		}
+
+		$hmacSha256 = New-Object System.Security.Cryptography.HMACSHA256
+		$hmacSha256.Key = [System.Convert]::FromBase64String($key)
+ 
+		$payLoad = "$($verb.ToLowerInvariant())`n$($resourceType.ToLowerInvariant())`n$resourceLink`n$($dateTime.ToLowerInvariant())`n`n"
+		$hashPayLoad = $hmacSha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($payLoad))
+		$signature = [System.Convert]::ToBase64String($hashPayLoad);
+ 
+		[System.Web.HttpUtility]::UrlEncode("type=$keyType&ver=$tokenVersion&sig=$signature")
+	}
 		
 	Write-Host "Resetting Document DB"
 

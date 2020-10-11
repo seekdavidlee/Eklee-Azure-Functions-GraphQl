7376c8f548273460b012115814d25a7f1442d3c6
David Lee <seekdavidlee@gmail.com>
2020-10-11 13:24:05 -0500

    debug

--- TestExample.ps1
+++ TestExample.ps1
@@ -9,18 +9,19 @@ npm install --save-dev azure-functions-core-tools@3
 npm install --save-dev newman
 Pop-Location
 
-$p = Start-Process -WorkingDirectory $Path\Examples\Eklee.Azure.Functions.GraphQl.Example\bin\$BuildConfig\netstandard2.0 -FilePath node_modules\.bin\func -ArgumentList "host start"
+Start-Process -WorkingDirectory $Path\Examples\Eklee.Azure.Functions.GraphQl.Example\bin\$BuildConfig\netstandard2.0 -FilePath node_modules\.bin\func -ArgumentList "host start" -RedirectStandardOutput $Path\output.txt -RedirectStandardError $Path\err.txt
 
 Start-Sleep -s 10
-
-$p | ConvertTo-Json
 
 $func = Get-Process -Name func
 
 if (!$func) {
 	Write-Host "func not found"
+	return
 } else {
 	Write-Host "func is found"
+	Get-Content -Path $Path\output.txt
+	Get-Content -Path $Path\err.txt
 }
 
 $reportFilePath = "$ReportDir/report.xml"
@@ -28,8 +29,3 @@ Push-Location $Path\Examples\Eklee.Azure.Functions.GraphQl.Example\bin\$BuildCon
 node_modules\.bin\newman run ..\..\..\..\..\tests\Eklee.Azure.Functions.GraphQl.postman_collection.json -e "$EnvironmentPath\Tests\Eklee.Azure.Functions.GraphQl.Local.postman_environment.json" --reporters 'cli,junit' --reporter-junit-export $reportFilePath
 Pop-Location
 
-$stdout = $func.StandardOutput.ReadToEnd()
-$stderr = $func.StandardError.ReadToEnd()
-Write-Host "stdout: $stdout"
-Write-Host "stderr: $stderr"
-Write-Host "exit code: " + $func.ExitCode

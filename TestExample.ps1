71b83e50ac7011f6b5354cee4a352541475f27b2
David Lee <seekdavidlee@gmail.com>
2020-11-22 10:44:24 -0600

    resource group

--- TestExample.ps1
+++ TestExample.ps1
@@ -17,10 +17,9 @@ Compress-Archive -Path "$WorkingDirectory\*" -DestinationPath "$WorkingDirectory
 
 az extension add -n application-insights
 
-az group create --location $Location --name $StackName --tags $Tags | Out-Null
-az monitor app-insights component create --app $StackName --location $Location --kind web -g $StackName --application-type web --tags $Tags | Out-Null
-az storage account create --resource-group $StackName --name $StackName --tags $Tags | Out-Null
-az functionapp create --consumption-plan-location $Location --name $StackName --os-type Windows --resource-group $StackName --runtime dotnet --storage-account $StackName --app-insights $StackName --tags $Tags | Out-Null
+az monitor app-insights component create --app $StackName --location $Location --kind web -g $Name --application-type web --tags $Tags | Out-Null
+az storage account create --resource-group $Name --name $StackName --tags $Tags | Out-Null
+az functionapp create --consumption-plan-location $Location --name $StackName --os-type Windows --resource-group $Name--runtime dotnet --storage-account $StackName --app-insights $StackName --tags $Tags | Out-Null
 
 $content = (Get-Content -Path "$WorkingDirectory\local.settings.json" | ConvertFrom-Json).Values
 

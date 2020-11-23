d292e97b46a2e4353aa0247dad689620431a7aa7
David Lee <seekdavidlee@gmail.com>
2020-11-23 07:32:41 -0600

    prepare for nuget pack

---
+++ PrepNugetPack.ps1
@@ -0,0 +1,9 @@
+param(
+    [Parameter(Mandatory = $True)][string]$Path)
+
+$app = "Eklee.Azure.Functions.GraphQl.dll"
+$proj = "Eklee.Azure.Functions.GraphQl\Eklee.Azure.Functions.GraphQl.csproj"
+
+Move-Item -Path "$Path\$proj\bin\Release\netstandard2.0\bin\$app.dll" -Destination "bin\Release\netstandard2.0\$app.dll"
+Remove-Item -Path "$Path\$proj\bin\Release\netstandard2.0\bin" -Recurse
+Copy-Item "$Path\LICENSE" "$Path\LICENSE.txt"

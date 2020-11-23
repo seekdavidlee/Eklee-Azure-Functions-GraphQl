6f370695c36171959680c28ba220c293af5f5819
David Lee <seekdavidlee@gmail.com>
2020-11-22 21:44:34 -0600

    nuget pack

--- TestExample.ps1
+++ TestExample.ps1
@@ -66,5 +66,5 @@ $content | Out-File "$Path\Tests\Eklee.Azure.Functions.GraphQl.Local.postman_env
 
 $reportFilePath = "$ReportDir/report.xml"
 Push-Location $Path\Examples\Eklee.Azure.Functions.GraphQl.Example\bin\$BuildConfig\netstandard2.0
-node_modules\.bin\newman run ..\..\..\..\..\tests\Eklee.Azure.Functions.GraphQl.postman_collection.json -e "$EnvironmentPath\Tests\Eklee.Azure.Functions.GraphQl.Local.postman_environment.json" --reporters 'cli,junit' --reporter-junit-export $reportFilePath
+node_modules\.bin\newman run ..\..\..\..\..\tests\Eklee.Azure.Functions.GraphQl.postman_collection.json -e "$EnvironmentPath\Tests\Eklee.Azure.Functions.GraphQl.Local.postman_environment.json" --reporters 'cli,junit' --reporter-junit-export $reportFilePath --delay-request 100
 Pop-Location

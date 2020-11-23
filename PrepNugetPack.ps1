b98c372b8867f06869b52a4b7491eb4b1edbfd8e
David Lee <seekdavidlee@gmail.com>
2020-11-23 17:08:04 -0600

    fix names

--- PrepNugetPack.ps1
+++ PrepNugetPack.ps1
@@ -1,9 +1,8 @@
 param(
     [Parameter(Mandatory = $True)][string]$Path)
 
-$app = "Eklee.Azure.Functions.GraphQl.dll"
-$proj = "Eklee.Azure.Functions.GraphQl"
+$app = "Eklee.Azure.Functions.GraphQl"
 
-Move-Item -Path "$Path\$proj\bin\Release\netstandard2.0\bin\$app.dll" -Destination "bin\Release\netstandard2.0\$app.dll"
-Remove-Item -Path "$Path\$proj\bin\Release\netstandard2.0\bin" -Recurse
+Move-Item -Path "$Path\$app\bin\Release\netstandard2.0\bin\$app.dll" -Destination "bin\Release\netstandard2.0\$app.dll"
+Remove-Item -Path "$Path\$app\bin\Release\netstandard2.0\bin" -Recurse
 Copy-Item "$Path\LICENSE" "$Path\LICENSE.txt"

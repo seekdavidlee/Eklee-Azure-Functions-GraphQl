3b4fa3bf67c12fbc0d7b8d087b1f61352b07b144
David Lee <seekdavidlee@gmail.com>
2020-11-23 08:06:23 -0600

    prep proj

--- PrepNugetPack.ps1
+++ PrepNugetPack.ps1
@@ -2,7 +2,7 @@ param(
     [Parameter(Mandatory = $True)][string]$Path)
 
 $app = "Eklee.Azure.Functions.GraphQl.dll"
-$proj = "Eklee.Azure.Functions.GraphQl\Eklee.Azure.Functions.GraphQl.csproj"
+$proj = "Eklee.Azure.Functions.GraphQl"
 
 Move-Item -Path "$Path\$proj\bin\Release\netstandard2.0\bin\$app.dll" -Destination "bin\Release\netstandard2.0\$app.dll"
 Remove-Item -Path "$Path\$proj\bin\Release\netstandard2.0\bin" -Recurse

e71fa62e7bae89b95a5967a32810a6abe54f43f5
David Lee <seekdavidlee@gmail.com>
2020-11-23 21:26:59 -0600

    rework file path

--- PrepNugetPack.ps1
+++ PrepNugetPack.ps1
@@ -2,7 +2,13 @@ param(
     [Parameter(Mandatory = $True)][string]$Path)
 
 $app = "Eklee.Azure.Functions.GraphQl"
+$filePath = "$Path\$app\bin\Release\netstandard2.0\bin\$app.dll"
 
-Move-Item -Path "$Path\$app\bin\Release\netstandard2.0\bin\$app.dll" -Destination "bin\Release\netstandard2.0\$app.dll"
+if (![System.IO.File]::Exists($filePath)){
+    Write-Host "Missing $filePath"
+    Get-ChildItem -Path "$Path\$app\bin\Release\netstandard2.0\bin"
+}
+
+Move-Item -Path $filePath -Destination "$Path\$app\bin\Release\netstandard2.0\$app.dll"
 Remove-Item -Path "$Path\$app\bin\Release\netstandard2.0\bin" -Recurse
 Copy-Item "$Path\LICENSE" "$Path\LICENSE.txt"

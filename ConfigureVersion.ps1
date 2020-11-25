ba1faad8e7de439dd4d155540ab024734a9dfedd
David Lee <seekdavidlee@gmail.com>
2020-11-24 18:59:22 -0600

    only build and create nuget package if on master branch, configure versioning

--- ConfigureVersion.ps1
+++ ConfigureVersion.ps1
@@ -1,70 +1,18 @@
 param(
-    [Parameter(Mandatory = $False)][string]$BuildConfiguration,	
-    [Parameter(Mandatory = $False)][string]$IncrementVersionType)
+    [Parameter(Mandatory = $True)][string]$RunId)
 
 # Note: We are following: https://semver.org/
 # Given a version number MAJOR.MINOR.PATCH, increment the:
 
 # MAJOR version when you make incompatible API changes,
-# MINOR version when you add functionality in a backwards-compatible manner, and
-# PATCH version when you make backwards-compatible bug fixes.
+# MINOR version when you add functionality in a backwards-compatible manner OR bugfixes
 
 $versions = (Get-Content version.txt).Split(".")
 $major = [int]$versions[0]
 $minor = [int]$versions[1]
-$patch = [int]$versions[2]
-	
-if (!$BuildConfiguration) {
-    $buildConfig = "Release"
-		
-    if (!$IncrementVersionType) {
-        # Assume we are doing a minor release
-        $minor = $minor + 1
-    }
-    else {
-        if ($IncrementVersionType.ToLower() -eq "major") {
-            $major = $major + 1
-            $minor = 0
-            $patch = 0
-        }
-
-        if ($IncrementVersionType.ToLower() -eq "minor") {
-            $minor = $minor + 1
-            $patch = 0
-        }
-
-        if ($IncrementVersionType.ToLower() -eq "patch") {
-            $patch = $patch + 1
-        }
-    }
-
-}
-else {
-    $buildConfig = $BuildConfiguration
-
-    if (!$IncrementVersionType) {
-        # Assume we are doing a patch release
-        $patch = $patch + 1
-    }
-    else {
-        if ($IncrementVersionType.ToLower() -eq "major") {
-            $major = $major + 1
-            $minor = 0
-            $patch = 0
-        }
-
-        if ($IncrementVersionType.ToLower() -eq "minor") {
-            $minor = $minor + 1
-            $patch = 0
-        }
-
-        if ($IncrementVersionType.ToLower() -eq "patch") {
-            $patch = $patch + 1
-        }		
-    }
-}
+$patch = [int]$RunId
 
 $version = "$major.$minor.$patch"
 
-Write-Host "##vso[task.setvariable variable=buildConfig;isOutput=true]$buildConfig"
+Write-Host "##vso[task.setvariable variable=buildConfig;isOutput=true]Release"
 Write-Host "##vso[task.setvariable variable=version;isOutput=true]$version"
